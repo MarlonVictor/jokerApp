9de88714bf503e60a05f7f3f3f01d46a4860c1f6
MarlonVictor <cmarlonvictor11@gmail.com>
2022-05-10 21:54:41 -0300

    Mobile style finished in header

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,15 @@
 "use strict";
-function init() {
-    console.log('Typescript');
-}
-init();
+// Nav Animation
+var navigationElement = document.querySelector('nav');
+var navigationClasses = ['w-screen', 'bg-primary', 'text-white'];
+document.body.onscroll = function () {
+    var _a, _b;
+    scrollY > 50
+        ? navigationElement === null || navigationElement === void 0 ? void 0 : (_a = navigationElement.classList).add.apply(_a, navigationClasses)
+        : navigationElement === null || navigationElement === void 0 ? void 0 : (_b = navigationElement.classList).remove.apply(_b, navigationClasses);
+};
+// Nav toggle
+var btnOpenNav = document.querySelector('[data-open]');
+var btnCloseNav = document.querySelector('[data-close]');
+btnOpenNav === null || btnOpenNav === void 0 ? void 0 : btnOpenNav.addEventListener('click', function () { return document.body.classList.add('menu-expanded'); });
+btnCloseNav === null || btnCloseNav === void 0 ? void 0 : btnCloseNav.addEventListener('click', function () { return document.body.classList.remove('menu-expanded'); });
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-function init() {
-    console.log('Typescript');
+// Nav Animation
+const navigationElement = document.querySelector('nav')
+const navigationClasses = ['w-screen', 'bg-primary', 'text-white']
+
+document.body.onscroll = () => {
+    scrollY > 50 
+        ? navigationElement?.classList.add(...navigationClasses)
+        : navigationElement?.classList.remove(...navigationClasses)
 }
 
-init()
+
+// Nav toggle
+const btnOpenNav = document.querySelector('[data-open]')
+const btnCloseNav = document.querySelector('[data-close]')
+
+btnOpenNav?.addEventListener('click', () => document.body.classList.add('menu-expanded'))
+btnCloseNav?.addEventListener('click', () => document.body.classList.remove('menu-expanded'))
